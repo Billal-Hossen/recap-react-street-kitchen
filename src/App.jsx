1f3d365c6375530c38ecbd23bf81d45e6b17c0b6
Billal-Hossen <mddipu867@gmail.com>
2024-09-26 13:51:25 +0600

    Complete Nav and Banner section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Banner from './components/Banner/Banner'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Banner />
+      <h2 className='text-3xl'>Hello bangladesh</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import banner from '../../assets/images/bannar.png'
+export default function Banner() {
+  return (
+    <div
+      className="h-[500px] bg-cover bg-center items-center text-white mx-10 mt-4 rounded-lg p-4 "
+      style={{ backgroundImage: `url(${banner})` }}
+    >
+      <div className='space-y-4 text-center'>
+        <h3 className=" text-3xl mt-64">Discover an exceptional cooking class tailored for you!</h3>
+        <p className=' text-center'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+        <div className='flex mx-4 justify-center gap-4'>
+          <button className='btn bg-red-500 rounded-full  text-white'>Explore Now</button>
+          <button className='btn bg-slate-200 rounded-full  text-black'>Explore Now</button>
+        </div>
+      </div>
+    </div>
+  )
+}
